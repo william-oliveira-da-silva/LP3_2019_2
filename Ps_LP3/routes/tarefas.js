68a864d80e44ea4dd7e331334ef2ab1982ed3fe9
william-oliveira-da-silva <wyll.hello@gmail.com>
2019-12-15 21:57:45 -0200

    ps

--- Ps_LP3/routes/tarefas.js
+++ Ps_LP3/routes/tarefas.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 
 router.get('/tarefas', controllers. recuperaTarefa);
-router.get('/tarefas/:id', Controller. recuperaTarefa);
+router.get('/tarefas/:id', controllers. recuperaTarefa);
 router.post('/tarefas',controllers.salvar);
 router.put('/tarefas', controllers.atualizar);
 router.delete('/tarefas/:id', controllers.remover);
